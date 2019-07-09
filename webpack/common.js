ffefbb782a16bbd0cc3724f86d221a0c0e0f48bd
guilhermethales <guilhermevtog@hotmail.com>
2019-07-08 21:22:28 -0300

    updating webpack to version 2 and versions of the packages

--- webpack/common.js
+++ webpack/common.js
@@ -1,46 +1,47 @@
 'use strict'
 
-const path = require('path')
+const { join } = require('path')
 
 module.exports = {
-  entry: path.join(__dirname, '..', 'src', 'index'),
+  entry: join(__dirname, '..', 'src', 'index'),
 
   output: {
-    path: path.join(__dirname, '..', 'dist'),
+    path: join(__dirname, '..', 'dist'),
     filename: '[name]-[hash].js'
   },
 
   htmlPluginConfig: {
     title: 'My app',
-    template: path.join(__dirname, '..', 'src', 'html', 'template.html')
+    template: join(__dirname, '..', 'src', 'html', 'template.html')
   },
 
   standardPreLoader: {
     test: /\.js$/,
+    enforce: 'pre',
     exclude: /node_modules/,
-    include: /src/,
-    loader: 'standard'
+    include: join(__dirname, '..', 'src'),
+    use: 'standard-loader'
   },
 
   jsLoader: {
     test: /\.js$/,
     exclude: /node_modules/,
-    include: /src/,
-    loader: 'babel'
+    include: join(__dirname, '..', 'src'),
+    use: 'babel-loader'
   },
 
   cssLoader: {
     test: /\.css$/,
     exclude: /node_modules/,
-    include: /src/,
-    loaders: ['style', 'css']
+    include: join(__dirname, '..', 'src'),
+    use: ['style-loader', 'css-loader']
   },
 
   resolve: {
     alias: {
-      src: path.join(__dirname, '..', 'src'),
-      components: path.join(__dirname, '..', 'src', 'components'),
-      utils: path.join(__dirname, '..', 'src', 'utils')
+      src: join(__dirname, '..', 'src'),
+      components: join(__dirname, '..', 'src', 'components'),
+      utils: join(__dirname, '..', 'src', 'utils')
     }
   }
 }
