17988626dda7916e1b5300416392662f8630f3c4
Guilherme Simões <guilhermevtog@hotmail.com>
2019-07-16 20:58:05 -0300

    Promises and async await

--- src/app.js
+++ src/app.js
@@ -1,14 +1,39 @@
 'use strict'
 
-import React from 'react'
+import React, { Component } from 'react'
 import Title from 'components/title'
 
 import './css/style.css'
 
-const App = () => (
-  <div>
-    <Title>My App</Title>
-  </div>
-)
+class App extends Component {
+  constructor () {
+    super()
+    this.state = {
+      title: '...'
+    }
+  }
+
+  getTitle () {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve('My App with Async Await')
+      }, 2000)
+    })
+  }
+
+  async componentDidMount () {
+    try {
+      this.setState({ title: await this.getTitle() })
+    } catch (error) {}
+  }
+
+  render () {
+    return (
+      <div>
+        <Title>{this.state.title}</Title>
+      </div>
+    )
+  }
+}
 
 export default App
--- src/promise.js
+++ src/promise.js
@@ -1,16 +1,25 @@
 const fs = require('fs')
 
-const readFile = (filePath, charset) => {
-  return new Promise((resolve, reject) => {
-    fs.readFile(filePath, charset, (err, result) => {
-      if (err) return reject(err)
-
-      return resolve(result)
-    })
+const readFile = (filePath, charset) => new Promise((resolve, reject) => {
+  fs.readFile(filePath, charset, (err, result) => {
+    if (err) return reject(err)
+    resolve(result)
   })
+})
+
+// readFile('../package.json', 'utf8')
+// .then((result) => JSON.parse(result).dependencies)
+// .then((dependencies) => console.log(dependencies))
+// .catch((error) => console.log(error))
+
+const requestAjax = async () => {
+  try {
+    const result = await readFile('./package.json', 'utf8')
+    const dependencies = await JSON.parse(result).dependencies
+    console.log(dependencies)
+  } catch (error) {
+    console.log('ERROR:', error)
+  }
 }
 
-readFile('../package.json', 'utf8')
-.then((result) => JSON.parse(result).dependencies)
-.then((dependencies) => console.log(dependencies))
-.catch((error) => console.log(error))
+requestAjax()
