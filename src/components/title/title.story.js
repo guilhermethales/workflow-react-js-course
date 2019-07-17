fcd56dbb24787774d7cf7ff94b4db0368b78356e
Guilherme Simões <guilhermevtog@hotmail.com>
2019-07-16 21:25:07 -0300

    centralizing webpack paths in common.js

--- src/components/title/title.story.js
+++ src/components/title/title.story.js
@@ -1,7 +1,7 @@
 'use strict'
 
 import { storiesOf } from '@kadira/storybook'
-import React from 'react'
+import React, { Component } from 'react'
 import Title from './index'
 
 const stories = storiesOf('Title', module)
@@ -9,3 +9,42 @@ const stories = storiesOf('Title', module)
 stories.add('without props', () => (
   <Title>Main title</Title>
 ))
+
+stories.add('Title with async / await', () => {
+  class App extends Component {
+    constructor () {
+      super()
+      this.state = {
+        title: '...'
+      }
+    }
+
+    getTitle () {
+      return new Promise((resolve, reject) => {
+        this.timer = setTimeout(() => {
+          resolve('My App with Async Await')
+        }, 2000)
+      })
+    }
+
+    async componentDidMount () {
+      try {
+        this.setState({ title: await this.getTitle() })
+      } catch (error) {}
+    }
+
+    componentWillUnmount () {
+      clearTimeout(this.timer)
+    }
+
+    render () {
+      return (
+        <div>
+          <Title>{this.state.title}</Title>
+        </div>
+      )
+    }
+  }
+
+  return <App />
+})
--- webpack/prod.config.js
+++ webpack/prod.config.js
@@ -1,6 +1,5 @@
 'use strict'
 
-const { join } = require('path')
 const webpack = require('webpack')
 
 const common = require('./common')
@@ -15,7 +14,7 @@ module.exports = {
 
   plugins: [
     new CleanPlugin(['dist', {
-      root: join(__dirname, '..')
+      root: common.paths.root
     }]),
     new ExtractTextPlugin({
       filename: '[name]-[hash].css'
